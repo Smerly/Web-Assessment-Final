5471f2b098a699ad0825d4f1e187b7c07eed4fd6
Smerly <69550699+Smerly@users.noreply.github.com>
2020-10-06 16:51:21 -0700

    Add files via upload

---
+++ Web-Assessment-Final/scripts.js
@@ -0,0 +1,34 @@
+const display = document.querySelector("#display");
+
+const inputSize = document.querySelector("#input-size");
+const showSize = document.querySelector("#show-size");
+
+const inputFont = document.querySelector("#select-font");
+const showFont = document.querySelector("#show-font");
+
+const inputColor = document.querySelector("#input-color");
+const showColor = document.querySelector("show-color");
+
+const inputBackgroundColor = document.querySelector("input-bg-color");
+const showBackgroundColor = document.querySelector("show-bg-color");
+
+const enterText = document.querySelector("enter-text");
+const showEnterText = document.querySelector("show-enter-text");
+
+inputSize.addEventListener("input", handleInput);
+inputFont.addEventListener("input", handleInput);
+inputColor.addEventListener("input", handleInput);
+inputBackgroundColor.addEventListener("input", handleInput);
+enterText.addEventListener("input", handleInput);
+
+function handleInput(e) {
+  const fontSize = inputSize.value + "px";
+  display.style.fontSize = fontSize;
+  showSize.innerHTML = fontSize;
+
+  const fontFamily = inputFont.value;
+  display.style.fontFamily = fontFamily;
+  showFont.innerHTML = fontFamily;
+}
+
+handleInput();
